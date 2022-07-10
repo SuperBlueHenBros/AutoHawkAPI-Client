c5668baf7a79d84417635b18c2ef3bed4907cf93
Maximillian Strand <maximillian.strand@protonmail.com>
2022-07-10 02:22:58 +0200

    updated message of error to include error code

--- bizhook/exceptions.py
+++ bizhook/exceptions.py
@@ -7,8 +7,9 @@ class InvalidRequest(BizhawkHookError):
     """Bizhawk was sent an invalid request"""
 
     def __init__(self, code, message):
-        super().__init__(message)
+        super().__init__(f'[{code}] {message}')
         self.code = code
+        self.message = message
 
 class InvalidResponse(BizhawkHookError):
     """Response did not follow the parseable format"""
